57f6f99997a703a35b014e4081f576d3cf542dd8
Acdaling <acdaling@gmail.com>
2020-09-19 19:10:48 +0200

    Random game title is been chosen when going to the next screen

--- app/src/main/java/com/example/madlevel3task1/RatingFragment.kt
+++ app/src/main/java/com/example/madlevel3task1/RatingFragment.kt
@@ -1,10 +1,11 @@
 package com.example.madlevel3task1
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_rating.*
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -12,8 +13,8 @@ import android.view.ViewGroup
 class RatingFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_rating, container, false)
@@ -21,8 +22,16 @@ class RatingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        showRandomAssessableGame()
     }
 
-    
+    private fun showRandomAssessableGame() {
+        val randomGame = listOf(
+            "Red Dead Redemption 2", "Rocket League",
+            "Shadow of the Tombraider"
+        ).random()
+
+        txtViewGameTitle.text = randomGame
+    }
+
 }
